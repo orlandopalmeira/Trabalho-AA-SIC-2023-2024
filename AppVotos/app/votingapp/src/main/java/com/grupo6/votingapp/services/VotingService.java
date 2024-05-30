ffb1bc06b3c04f747d5f758fcc47104e59f4a663
orlandopalmeira <orlando.palmeira3@gmail.com>
2024-05-30 15:53:38 +0100

    limpeza ao votingservice

--- AppVotos/app/votingapp/src/main/java/com/grupo6/votingapp/services/VotingService.java
+++ AppVotos/app/votingapp/src/main/java/com/grupo6/votingapp/services/VotingService.java
@@ -25,7 +25,10 @@ public class VotingService {
         this.userRepository = userRepository;
     }
 
-    //* CRUD
+    public Voting getAccessibleVotingToUser(String votingId, String userId){ //* Parece funcionar
+        return votingRepository.findAccessibleVotingToUser(userId, Long.parseLong(votingId)).orElse(null);
+    }
+
     public List<Voting> getAccessibleVotingsToUser(String userId){ //* Parece funcionar
         return votingRepository.findAccessibleVotingsToUser(userId);
     }
@@ -41,48 +44,7 @@ public class VotingService {
         return saveVoting(voting);//* Guarda a votação na base de dados
     }
 
-    public Voting updateVoting(Voting newVoting){ //* Parece funcionar (!!ver o TO DO!!)
-        Voting oldVoting = votingRepository.findById(newVoting.getId()).orElse(null);
-        if(oldVoting != null){
-            if(newVoting.getTitle() != null)       oldVoting.setTitle(newVoting.getTitle());
-            if(newVoting.getDescription() != null) oldVoting.setDescription(newVoting.getDescription());
-            if(newVoting.getImage() != null)       oldVoting.setImage(newVoting.getImage());
-            if(newVoting.getEnddate() != null)     oldVoting.setEnddate(newVoting.getEnddate());
-            // TODO: se passar de privada a pública, devemos apagar esta votação da tabela M:N que associa votações privadas a votantes (utilizadores) inscritos nela (tabela private_voters)
-            oldVoting.setPrivatevoting(newVoting.getPrivatevoting());
-            return votingRepository.save(oldVoting);
-        }
-        return null;
-    }
-
-    public void deleteVoting(Long id){ //* Parece funcionar
-        Voting voting = votingRepository.findById(id).orElse(null);
-        if(voting != null){
-            voting.removeAllPrivateVoters();
-            votingRepository.delete(voting);
-        }
-    }
-
-    public void deleteVoting(String id){ //* Parece funcionar
-        deleteVoting(Long.parseLong(id));
-    }
-
-    public Voting getFromCreatorIdAndVotingId(String userId, String votingId){ //* Parece funcionar	
-        return votingRepository.findByUserIdAndVotingId(userId, votingId).orElse(null);
-    }
-
     public List<Voting> getVotingsFromCreatorId(String userId){ //* Parece funcionar
         return votingRepository.findByUserId(userId);
     }
-
-    public Voting setPrivateVoters(String voting_id, List<Long> privateVotersIds){ //* Parece funcionar
-        Voting votingInDB = votingRepository.findById(Long.parseLong(voting_id)).orElse(null);
-        if(votingInDB != null){
-            List<User> privateVoters = userRepository.findByIds(privateVotersIds); //! VER SE IMPLICA CARGA NA BD
-            votingInDB.setPrivatevoters(privateVoters);
-            return votingRepository.save(votingInDB);
-        }
-        return null;
-    }
-    
 }
