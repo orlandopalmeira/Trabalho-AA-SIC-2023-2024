f04373bd97e97489fde5d936271ef0e76a7b2bcc
orlandopalmeira <orlando.palmeira3@gmail.com>
2024-05-30 14:35:52 +0100

    testes google cloud comentados

--- AppVotos/app/votingapp/src/test/java/com/grupo6/votingapp/GcsTest.java
+++ AppVotos/app/votingapp/src/test/java/com/grupo6/votingapp/GcsTest.java
@@ -18,43 +18,43 @@ public class GcsTest {
     @Autowired
     private GcsService gcsService;
 
-    @Test
-    public void testDownload() {
-        // Assuming MyService has a method called someMethod() that you want to test
-        String imageName = "kitten.png";
-        byte[] result = gcsService.downloadImage(imageName);
-        // Write the image to a file
-        try {
-            String filePath = "image-ret.png";
-            FileOutputStream fos = new FileOutputStream(filePath);
-            fos.write(result);
-            fos.close();
-        } catch (IOException e) {
-            // Handle the exception
-            e.printStackTrace();
-        }
+    // @Test
+    // public void testDownload() {
+    //     // Assuming MyService has a method called someMethod() that you want to test
+    //     String imageName = "kitten.png";
+    //     byte[] result = gcsService.downloadImage(imageName);
+    //     // Write the image to a file
+    //     try {
+    //         String filePath = "image-ret.png";
+    //         FileOutputStream fos = new FileOutputStream(filePath);
+    //         fos.write(result);
+    //         fos.close();
+    //     } catch (IOException e) {
+    //         // Handle the exception
+    //         e.printStackTrace();
+    //     }
 
-    }
+    // }
 
-    @Test
-    public void testDownloadFromFolder() {
-        // Assuming MyService has a method called someMethod() that you want to test
-        String folder = "profile";
-        String imageName = "image.png";
-        String path = folder + "/" + imageName;
-        byte[] result = gcsService.downloadImage(path);
-        // Write the image to a file
-        try {
-            String filePath = "image-ret.png";
-            FileOutputStream fos = new FileOutputStream(filePath);
-            fos.write(result);
-            fos.close();
-        } catch (IOException e) {
-            // Handle the exception
-            e.printStackTrace();
-        }
+    // @Test
+    // public void testDownloadFromFolder() {
+    //     // Assuming MyService has a method called someMethod() that you want to test
+    //     String folder = "profile";
+    //     String imageName = "image.png";
+    //     String path = folder + "/" + imageName;
+    //     byte[] result = gcsService.downloadImage(path);
+    //     // Write the image to a file
+    //     try {
+    //         String filePath = "image-ret.png";
+    //         FileOutputStream fos = new FileOutputStream(filePath);
+    //         fos.write(result);
+    //         fos.close();
+    //     } catch (IOException e) {
+    //         // Handle the exception
+    //         e.printStackTrace();
+    //     }
 
-    }
+    // }
 
     // @Test
     // public void TestUpload() {
