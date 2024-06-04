506b4659b439ae63d86c818245af7f9247271141
orlandopalmeira <orlando.palmeira3@gmail.com>
2024-06-04 18:35:58 +0100

    Alteração ao modo de obtenção de estatísticas de Votações

--- AppVotos/app/votingapp/src/main/java/com/grupo6/votingapp/dtos/stats/VotingStatsDTO.java
+++ AppVotos/app/votingapp/src/main/java/com/grupo6/votingapp/dtos/stats/VotingStatsDTO.java
@@ -1,41 +1,24 @@
 package com.grupo6.votingapp.dtos.stats;
 
 import java.util.List;
+import java.util.Map;
 
 import com.grupo6.votingapp.dtos.users.UsersWithNoRelationsDTO;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class VotingStatsDTO {
     private Long numvotes;
-    private List<OptionStats> optionStats;
+    private Map<Long, List<OptionStats>> optionStats;
     private List<UsersWithNoRelationsDTO> voters;
 
-    public VotingStatsDTO(CountVotesOfVoting countVotesOfVoting, List<OptionStats> optionStats, List<UsersWithNoRelationsDTO> voters) {
-        this.numvotes = countVotesOfVoting.getNumvotes();
+    public VotingStatsDTO(Long numVotes, Map<Long, List<OptionStats>> optionStats, List<UsersWithNoRelationsDTO> voters) {
+        this.numvotes = numVotes;
         this.optionStats = optionStats;
         this.voters = voters;
     }
 
-    public Long getNumvotes() {
-        return numvotes;
-    }
-
-    public void setCountVotesOfVoting(Long numvotes) {
-        this.numvotes = numvotes;
-    }
-
-    public List<OptionStats> getOptionStats() {
-        return optionStats;
-    }
-
-    public void setOptionStats(List<OptionStats> optionStats) {
-        this.optionStats = optionStats;
-    }
-
-    public List<UsersWithNoRelationsDTO> getVoters() {
-        return voters;
-    }
-
-    public void setVoters(List<UsersWithNoRelationsDTO> voters) {
-        this.voters = voters;
-    }
 }
--- AppVotos/app/votingapp/src/main/java/com/grupo6/votingapp/repositories/StatsRepository.java
+++ AppVotos/app/votingapp/src/main/java/com/grupo6/votingapp/repositories/StatsRepository.java
@@ -2,12 +2,13 @@ package com.grupo6.votingapp.repositories;
 
 import java.sql.Date;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.stereotype.Repository;
 
-import com.grupo6.votingapp.dtos.stats.CountVotesOfVoting;
 import com.grupo6.votingapp.dtos.stats.OptionStats;
 import com.grupo6.votingapp.dtos.users.UsersWithNoRelationsDTO;
 
@@ -21,61 +22,52 @@ public class StatsRepository {
         this.entityManager = entityManager;
     }
 
-    public Optional<CountVotesOfVoting> getCountVotesOfVoting(Long votingId) {
-        String query = "select v.voting_id as `id`, count(v.voting_id) as `num_votes` from votes v where v.voting_id = ?1;";
+    public Long getCountVotesOfVoting(Long votingId) {
+        String query = "select count(*) from votes where voting_id = ?1;";
         var result = entityManager.createNativeQuery(query)
                                   .setParameter(1, votingId)
                                   .getResultList();
-        if (!result.isEmpty()) {
-            Object[] row = (Object[]) result.get(0);
-            if(row[0] != null && row[1] != null){
-                return Optional.of(new CountVotesOfVoting((Long) row[0], (Long) row[1]));
-            } else {
-                return Optional.empty();
-            }
-        }
-        return Optional.empty();
+        return (Long) result.get(0);
     }
 
-    public Optional<CountVotesOfVoting> getCountVotesOfVoting(String votingId) {
+    public Long getCountVotesOfVoting(String votingId) {
         return getCountVotesOfVoting(Long.parseLong(votingId));
     }
 
-    public Optional<List<CountVotesOfVoting>> getCountVotesOfVotings(List<Long> votingsIds) {
-        String query = "select v.voting_id as `id`, count(v.voting_id) as `num_votes` from votes v where v.voting_id in ?1 group by v.voting_id";   
+    public Map<Long,Long> getCountVotesOfVotings(List<Long> votingsIds) {
+        String query = "select v.voting_id, count(v.voting_id) from votes v where v.voting_id in ?1 group by v.voting_id;";   
         var result = entityManager.createNativeQuery(query)
                                   .setParameter(1, votingsIds)
                                   .getResultList();
+        Map<Long, Long> countVotesOfVotings = new HashMap<>(); //* formato: { votingId: number of votes }
         if (!result.isEmpty()) {
-            List<CountVotesOfVoting> countVotesOfVoting = new ArrayList<>();
             for (Object row : result) {
                 Object[] r = (Object[]) row;
-                countVotesOfVoting.add(new CountVotesOfVoting((Long) r[0], (Long) r[1]));
+                countVotesOfVotings.put((Long) r[0], (Long) r[1]);
             }
-            return Optional.of(countVotesOfVoting);
         }
-        return Optional.of(List.of());
+        return countVotesOfVotings;
     }
 
-    public Optional<List<OptionStats>> getOptionStats(Long votingId) {
-        String query = "select o.id, o.description, o.image, coalesce(count(vqo.option_id),0) as `count` from options o left join votes_questions_options vqo on o.id = vqo.option_id where o.id in (select o.id from questions q join options o on q.id= o.question_id where q.voting_id=?1) group by o.id";
+    public Map<Long,List<OptionStats>> getOptionStats(Long votingId) {
+        String query = "select o.id, o.question_id, o.description, o.image, coalesce(count(vqo.option_id),0) from options o left join votes_questions_options vqo on o.id = vqo.option_id where o.id in (select o.id from questions q join options o on q.id= o.question_id where q.voting_id=?1) group by o.id";
         var result = entityManager.createNativeQuery(query)
                                   .setParameter(1, votingId)
                                   .getResultList();
 
+        Map<Long, List<OptionStats>> optionStats = new HashMap<>(); //* formato: { questionId: [OptionStats] }
         if (!result.isEmpty()) {
-            List<OptionStats> optionStats = new ArrayList<>();
             for (Object row : result) {
                 Object[] r = (Object[]) row;
-                optionStats.add(new OptionStats((Long) r[0], (String) r[1], (String) r[2], (Long) r[3]));
+                Long questionId = (Long) r[1];
+                optionStats.computeIfAbsent(questionId, k -> new ArrayList<>()); // Cria uma lista para a questionId caso ela não exista
+                optionStats.get(questionId).add(new OptionStats((Long) r[0], (String) r[2], (String) r[3], (Long) r[4]));
             }
-            return Optional.of(optionStats);
         }
-
-        return Optional.of(List.of());
+        return optionStats;
     }
 
-    public Optional<List<OptionStats>> getOptionStats(String votingId) {
+    public Map<Long,List<OptionStats>> getOptionStats(String votingId) {
         return getOptionStats(Long.parseLong(votingId));
     }
 
