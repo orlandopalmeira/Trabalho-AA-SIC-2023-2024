10d9588e22dc5da26076d322eababbcf65a4b496
orlandopalmeira <orlando.palmeira3@gmail.com>
2024-06-04 22:23:31 +0100

    Tab de estatísticas já solicita estatísticas ao servidor e apresenta-as

--- Interface/src/components/Voting/StatsTab.vue
+++ Interface/src/components/Voting/StatsTab.vue
@@ -1,9 +1,15 @@
 <template>
-    <div class="background ma-5 pa-2">
+    <div v-if="loadingStats" class="ma-5 pa-2">
+        <LoadingAlert message="A obter as estatísticas, por favor aguarde..." />
+    </div>
+    <div v-else-if="stats.numvotes === 0" class="ma-5 pa-2">
+        <SimpleAlert message="Esta votação ainda não tem votos." />
+    </div>
+    <div v-else class="background ma-5 pa-2">
         <div style="width: 25%;margin: 10px 10px;" class="mb-5">
             <v-select
                 v-model="selected"
-                :items="['Pergunta 1', 'Pergunta 2', 'Pergunta 3']"
+                :items="selectItems"
                 density="compact"
                 class="select-question"
                 variant="outlined"
@@ -14,12 +20,12 @@
             <v-row>
                 <v-col class="vcol1">
                     <p class="text-h6" style="font-weight: bold;">Número de votos</p>
-                    <p style="font-size: 20pt;">293847</p>
+                    <p style="font-size: 20pt;">{{ stats.numvotes }}</p>
                 </v-col>
                 <v-col class="vcol1">
                     <p class="text-h6" style="font-weight: bold;">Mais votado</p>
-                    <p style="font-size: 20pt;">AD</p>
-                    <p>Com 12345 votos</p>
+                    <p style="font-size: 20pt;">{{ winner.description }}</p>
+                    <p>Com {{ winner.count }} votos</p>
                 </v-col>
                 <v-col class="vcol1">
                     <p class="text-h6" style="font-weight: bold;">???</p>
@@ -27,29 +33,28 @@
                 </v-col>
             </v-row>
             <v-row>
-                <v-col style="max-height: calc(100vh - 250px); overflow-y: auto;" class="vcol1 pl-5" cols="4">
+                <v-col class="vcol1 pl-5 votantes" cols="4">
                     <p class="text-h6" style="font-weight: bold;">Votantes</p>
-                    <!--Isto é para ser um v-for-->
-                    <v-row v-for="i in 10">
+                    <v-row v-for="(voter, index) in stats.voters" :key="index">
                         <v-col class="center" cols="1"><v-icon size="x-large">mdi-account-circle</v-icon></v-col>
                         <v-col>
-                            <p>Nome do votante</p>
-                            <p style="color: #454545">Email do votante</p>
+                            <p>{{ voter.name }}</p>
+                            <p style="color: #454545">{{ voter.email }}</p>
                         </v-col>
                     </v-row>
                 </v-col>
                 <v-col class="vcol1" >
-                    <Bar
-                        id="my-chart-id"
-                        :data="chart.data"
-                        :options="chart.options"
-                    />
+                    <Bar id="my-chart-id" :data="chart.data" :options="chart.options" />
                 </v-col>
             </v-row>
         </div>
     </div>
 </template>
 <script>
+import LoadingAlert from '../LoadingAlert.vue';
+import SimpleAlert from '../SimpleAlert.vue';
+import axios from '@/axios';
+
 import { Bar } from 'vue-chartjs'
 import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
 
@@ -57,26 +62,61 @@ ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
 
 export default {
     components: {
-        Bar
+        Bar,
+        LoadingAlert,
+        SimpleAlert
+    },
+    props: {
+        votingId: { type: String, required: true }
     },
     data() {
         return {
-            selected: "Pergunta 1",
-            chart: {
+            loadingStats: true,
+            selected: null,
+            selectItems: null,
+            stats: null,
+        }
+    },
+    computed: {
+        chart(){
+            let question = this.stats.questionsstats[this.selected];
+            return {
                 data: {
-                    labels: [ 'January', 'February', 'March', 'April', 'May' ],
+                    labels: question.options.map(option => option.description),
                     datasets: [{
                         label: 'Quantidade de votos',
-                        backgroundColor: '#000000',
-                        data: [40, 20, 12, 26, 32] 
+                        backgroundColor: '#000000', // cor das barras do gráfico
+                        data: question.options.map(option => option.count)
                     }]
                 },
                 options: {
                     responsive: true
                 }
             }
+        },
+        winner(){
+            let options = this.stats.questionsstats[this.selected].options;
+            let winner = options.reduce((op1, op2) => op1.count > op2.count ? op1 : op2); // lidamos com empates?
+            return winner;
         }
     },
+    created() {
+        axios.get(`/votings/${this.votingId}/stats`)
+            .then(response => {
+                this.stats = response.data;
+                this.selectItems = this.stats.questionsstats.map((qs,index) => ({
+                    title: `Pergunta ${index+1}: "` + qs.description + '"',
+                    value: index
+                }));
+                this.selected = 0;
+                this.loadingStats = false;
+            })
+            .catch(error => {
+                console.error(error);
+                this.loadingStats = false;
+            });
+    },
+
 }
 </script>
 <style>
@@ -97,5 +137,12 @@ export default {
     display: flex;
     justify-content: center;
     align-items: center;
+}
+.votantes {
+    max-height: calc(100vh - 250px);
+    overflow-y: hidden;
+}
+.votantes:hover {
+    overflow-y: auto; /* Show the scrollbar on hover */
 }
 </style>
