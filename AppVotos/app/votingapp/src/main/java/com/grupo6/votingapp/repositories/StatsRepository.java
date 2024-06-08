5340099480395296fab375864ade311835e221f4
orlandopalmeira <orlando.palmeira3@gmail.com>
2024-06-08 13:46:05 +0100

    Refactor StatsRepository to remove unused import

--- AppVotos/app/votingapp/src/main/java/com/grupo6/votingapp/repositories/StatsRepository.java
+++ AppVotos/app/votingapp/src/main/java/com/grupo6/votingapp/repositories/StatsRepository.java
@@ -6,7 +6,6 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Repository;
